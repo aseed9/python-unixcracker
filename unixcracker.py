dd42d48d663c410bfa28f001b2e1ea18ada62731
d3 <dexray@live.com>
2018-05-14 18:55:46 +0300

    Some progress

--- unixcracker.py
+++ unixcracker.py
@@ -1,9 +1,16 @@
-import crypt
+import hashlib
  
-def testPass(cryptpass):
+def testPass(passwd):
     dictFile = open('dictionary.txt', 'r')
+    semi_c = ':'
+    d_sign = '$'
+    pwdlist = passwd.split("$")
+    salt ='$'+pwdlist[1]+'$'+pwdlist[2]
+    cryptPas = passwd.split(d_sign, 3)[3]
+    cryptPass = cryptPas.split(semi_c)[0]
     for word in dictFile:
-        cryptWord = hashlib.sha512(salt.encode() + word.encode()).hexdigest() + ': ' + salt
+        cryptWord = hashlib.sha512(salt.encode() + word.encode()).hexdigest() + ':' + salt
+        print cryptWord
         if (cryptWord == cryptPass):
             print '[+] Found Password : ' + word + '\n'
             return
@@ -14,14 +21,11 @@ def main():
     passfile = open('password.txt')
     passwd = passfile.readline()
     semi_c = ':'
-    d_sign = '$'
     print passwd
     if semi_c in passwd:
             user = passwd.split(semi_c)[0]
-            salt = 
-            cryptPass = 
             print '[*] Cracking Password for : ' + user
-            testPass(cryptPass)
+            testPass(passwd)
 
 if __name__ == '__main__':
         main() 
